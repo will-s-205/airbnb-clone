cb921b2d33dcdf40270cbb870b8cfeeb3018afa7
William S <will.step.205@gmail.com>
2023-06-13 16:48:24 -0700

    UI changes added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Navbar from '../src/components/Navbar'
+import Hero from '../src/components/Hero'
 
 function App() {
   return (
     <div>
       <Navbar />
+      <Hero />
     </div>
   );
 }
---
+++ src/components/Hero.js
@@ -0,0 +1,12 @@
+import group_photo from '../images/Group 77.png'
+
+export default function Hero() {
+    return (
+        <hero className="hero-section">
+            <img src={group_photo} className="hero--photos" alt="group of images" />
+            <h1 className="hero--header">Online Experiences</h1>
+            <p className="hero--paragraph">Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.
+            </p>
+        </hero>
+    )
+}
