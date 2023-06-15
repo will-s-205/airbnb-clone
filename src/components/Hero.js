a49c7fedb0317a3c706e85c93ac79639620488ae
William S <will.step.205@gmail.com>
2023-06-15 14:48:59 -0700

    class card added

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,10 +3,12 @@ import group_photo from '../images/Group 77.png'
 export default function Hero() {
     return (
         <hero className="hero-section">
-            <img src={group_photo} className="hero--photos" alt="group of images" />
-            <h1 className="hero--header">Online Experiences</h1>
-            <p className="hero--paragraph">Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.
-            </p>
+            <div className="card">
+                <img src={group_photo} className="hero--photos" alt="group of images" />
+                <h1 className="hero--header">Online Experiences</h1>
+                <p className="hero--paragraph">Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.
+                </p>
+            </div>
         </hero>
     )
 }
