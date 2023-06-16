0af196b332a219bf06ab88139227210fea2b457e
William S <will.step.205@gmail.com>
2023-06-16 14:42:34 -0700

    removing redundant lines of code

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,8 +4,6 @@ export default function Card(props) {
     return (
         <div className="card--section">
             <img src={props.img} className="card-img" alt="card-img" />
-            {/* JS interpolation below requier a backticks to run JS syntax in cirly braces*/}
-            {/* <img src={`./images/${props.img}`} className="card-img" alt="card-img" /> */}
             <div className="card--stats">
                 <img src={star} className="card--star" alt="star" />
                 <span>{props.rating}</span>
